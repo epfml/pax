264132b0af5bab2262e3678b69c8eeaba7cfce22
Thijs Vogels <thijs.vogels@epfl.ch>
2021-08-24 13:27:19 +0200

    added install instructions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="pax",
+    name="paxlib",
     version="0.1",
     description="JAX-like API on top of PyTorch",
     url="http://github.com/epfml/pax",
