10f7f3b7d992f8373f54b3a5a8a547de3f057a10
Thijs Vogels <thijs.vogels@epfl.ch>
2022-02-16 10:30:26 +0100

    pip package for pax.tasks

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="paxlib",
-    version="0.3.3",
+    version="0.3.5",
     description="JAX-like API on top of PyTorch",
     url="http://github.com/epfml/pax",
     author="Thijs Vogels",
     author_email="thijs.vogels@epfl.ch",
     license="MIT",
-    packages=["pax"],
+    packages=find_packages(),
     zip_safe=False,
     install_requires=[
           "torch",
